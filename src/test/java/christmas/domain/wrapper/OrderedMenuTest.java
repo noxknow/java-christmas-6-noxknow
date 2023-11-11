00d0802fe3b67e3b4e0a78e8d2c6d6f4932a8705
noxknow <chris2769@naver.com>
2023-11-11 12:14:16 +0900

    test(OrderedMenuTest) : 메뉴와 개수가 정상적으로 반환된다

--- src/test/java/christmas/domain/wrapper/OrderedMenuTest.java
+++ src/test/java/christmas/domain/wrapper/OrderedMenuTest.java
@@ -3,11 +3,26 @@ package christmas.domain.wrapper;
 import christmas.handler.ErrorHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class OrderedMenuTest {
+
+    private static Stream<Arguments> testMenu() {
+        return Stream.of(
+                Arguments.of(OrderedMenu.from("해산물파스타-1,레드와인-2"), Map.of("해산물파스타", 1,
+                        "레드와인", 2)),
+                Arguments.of(OrderedMenu.from("타파스-3,제로콜라-2"), Map.of("타파스", 3,
+                        "제로콜라", 2))
+        );
+    }
 
     @DisplayName("메뉴 형식이 다른 경우 예외가 발생한다.")
     @ParameterizedTest(name = "[{index}] input {0}")
@@ -43,5 +58,12 @@ public class OrderedMenuTest {
         assertThatThrownBy(() -> OrderedMenu.from(menuValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ErrorHandler.INVALID_MENU.getException().getMessage());
+    }
+
+    @DisplayName("메뉴와 개수가 정상적으로 반환된다.")
+    @ParameterizedTest(name = "[{index}] input {0}")
+    @MethodSource("testMenu")
+    void createMenu(OrderedMenu orderedMenu, Map<String, Integer> expectedMenu) {
+        assertThat(orderedMenu.getOrderedMenu()).isEqualTo(expectedMenu);
     }
 }
