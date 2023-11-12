55780f70fafc18709d6865a8b4bf0d9e4fce444d
noxknow <chris2769@naver.com>
2023-11-12 23:32:29 +0900

    test(DiscountResultTest) : 평일 할인이 정상적으로 적용된다

--- src/test/java/christmas/domain/DiscountResultTest.java
+++ src/test/java/christmas/domain/DiscountResultTest.java
@@ -25,6 +25,19 @@ public class DiscountResultTest {
         );
     }
 
+    private static Stream<Arguments> testWeekDayDiscount() {
+        return Stream.of(
+                Arguments.of(5, Map.of("타파스", 1,
+                        "제로콜라", 1), 0),
+                Arguments.of(5, Map.of("초코케이크", 1,
+                        "바비큐립", 2), 2023),
+                Arguments.of(5,Map.of("아이스크림", 2,
+                        "시저샐러드", 1), 4046),
+                Arguments.of(5, Map.of("초코케이크", 1,
+                        "아이스크림", 2), 6069)
+        );
+    }
+
     @DisplayName("크리스마스 디데이 할인이 정상적으로 적용된다.")
     @ParameterizedTest(name = "[{index}] input {0}")
     @MethodSource("testChristmasDiscount")
@@ -32,5 +45,14 @@ public class DiscountResultTest {
         DiscountResult discountResult = DiscountResult.of(date, orderedMenu);
 
         assertThat(discountResult.christmasDiscount()).isEqualTo(expectedDiscount);
+    }
+
+    @DisplayName("평일 할인이 정상적으로 적용된다.")
+    @ParameterizedTest(name = "[{index}] input {0}")
+    @MethodSource("testWeekDayDiscount")
+    void createWeekDayDiscount(int date, Map<String, Integer> orderedMenu, int expectedDiscount) {
+        DiscountResult discountResult = DiscountResult.of(date, orderedMenu);
+
+        assertThat(discountResult.weekDayDiscount()).isEqualTo(expectedDiscount);
     }
 }
